8670e688cba222f59ba91d3c9c448d7a7ffd31af
tkhm <no-reply@tkhm.dev>
2014-11-05 13:03:42 +0900

    JSDocモジュールの追加及びそれに伴うqunitフォルダの移動

--- pushme/platforms/android/ant-build/PushMe.ap_.d
+++ pushme/platforms/android/ant-build/PushMe.ap_.d
@@ -35,7 +35,6 @@ C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\img \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\index.html \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\js \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\qunit \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\views \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\css\accordion.css \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\css\extend-pure.css \
@@ -52,10 +51,6 @@ C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\js\index.js \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\js\listclips.js \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\js\side_menu.js \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\qunit\qunit.css \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\qunit\qunit.js \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\qunit\tests.html \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\qunit\tests.js \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\views\decide.html \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\views\error.html \
 C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\assets\www\views\index.html \
