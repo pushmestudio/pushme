a7d8dc891ea0dd6c4d73c82fbd3bc51f74f4da17
tkhm <devtkhm@gmail.com>
2014-11-15 00:39:08 +0900

    共有によって最終決定結果を共有できることを確認 refs #81
    但し、テスト未実施であることや、共有時の文章フォーマットなどは要検討

---
+++ pushme/platforms/android/assets/www/js/share.js
@@ -0,0 +1,20 @@
+﻿/**
+ * 他のアプリにインテントを渡しデータを共有する。
+ * 共有のためのデータの整形も行う。
+ * @param {String} txt 共有したいテキストデータ
+ */
+function shareText(txt){
+    Bridge.shareText(formatForSend(txt));// native定義の機能を呼び出す
+}
+
+
+/**
+ * データを送信用に整形する。
+ * @param {String} txt 共有したいテキストデータ
+ * @return {String} formattedTxt 整形済テキストデータ
+ */
+ function formatForSend(txt){
+    var formattedTxt = "";
+    formattedTxt = "今回決まったのは [" + txt + "] です！";
+    return formattedTxt;
+ }
