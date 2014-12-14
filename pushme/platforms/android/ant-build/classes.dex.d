4cdb02d35d1070d08d4d33c23cd52875e53d1161
capsmalt <capsmalt@gmail.com>
2014-12-14 13:52:20 +0900

    before merge getFromDB

--- pushme/platforms/android/ant-build/classes.dex.d
+++ pushme/platforms/android/ant-build/classes.dex.d
@@ -1,23 +1,17 @@
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes.dex : \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\BuildConfig.class \
-<<<<<<< HEAD
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\PushMe$JSHandler.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\PushMe$MyWebViewClient.class \
-=======
->>>>>>> EditRegItem
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\PushMe.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\R$attr.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\R$drawable.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\R$string.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\R$xml.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\co\push\R.class \
-<<<<<<< HEAD
-=======
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$1.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$Action.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$DBQuery.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$DBRunner.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$QueryType.class \
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin.class \
->>>>>>> EditRegItem
-C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\ant-build\dexedLibs\classes-68297adb89e8d859f2fa7e54b317e727.jar \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes.dex : \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/BuildConfig.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/PushMe$JSHandler.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/PushMe$MyWebViewClient.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/PushMe.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/R$attr.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/R$drawable.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/R$string.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/R$xml.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/co/push/R.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$1.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$Action.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$DBQuery.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$DBRunner.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$QueryType.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/dexedLibs/classes-190f03605de7dc7831bbef249af80ae0.jar \
