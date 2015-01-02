27904c2b07930e1fda9f49983cdc3889e0640ba1
tkhm <no-reply@tkhm.dev>
2015-01-02 17:56:23 +0900

    表示件数を変更機能の追加(NOT cookie) refs #80

--- pushme/www/js/share.js
+++ pushme/www/js/share.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * @fileOverview 他のアプリケーションとの連携に必要な処理を実施します。
  */
 
@@ -8,7 +8,7 @@
  * @param {String} txt 共有したいテキストデータ
  */
 function shareText(txt){
-    Bridge.shareText(formatForSend(txt));// native定義の機能を呼び出す
+  Bridge.shareText(formatForSend(txt));// native定義の機能を呼び出す
 }
 
 /**
@@ -16,8 +16,8 @@ function shareText(txt){
  * @param {String} txt 共有したいテキストデータ
  * @return {String} formattedTxt 整形済テキストデータ
  */
- function formatForSend(txt){
-    var formattedTxt = "";
-    formattedTxt = "今回決まったのは [" + txt + "] です！";
-    return formattedTxt;
- }
+function formatForSend(txt){
+  var formattedTxt = "";
+  formattedTxt = "今回決まったのは [" + txt + "] です！";
+  return formattedTxt;
+}
