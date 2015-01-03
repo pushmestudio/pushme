53b610d263d86f341547ca1b89d1e9ab359888c1
capsmalt <capsmalt@gmail.com>
2015-01-03 09:44:09 +0900

    Review complete : editItem

--- pushme/platforms/android/ant-build/PushMe-debug-unaligned.apk.d
+++ pushme/platforms/android/ant-build/PushMe-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/PushMe-debug-unaligned.apk : \
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/PushMe.ap_ \
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/ant-build/classes.dex \
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/pushme/decision2014.pushme/pushme/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/ant-build/PushMe-debug-unaligned.apk : \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/ant-build/PushMe.ap_ \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/ant-build/classes.dex \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/CordovaLib/ant-build/classes.jar \
