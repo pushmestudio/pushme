1292ca2bcc4475c3014bd31790afa003a3ec8a4d
meltest7 <meltest7@gmail.com>
2015-01-25 12:18:07 +0900

    deferred/prmise 対応完了 refs #165

--- pushme/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ pushme/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/capsmalt/Documents/env/01.Projects/KSSOSS/review_editFunc/decision2014.pushme/pushme/platforms/android/CordovaLib/AndroidManifest.xml \
+C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\melty\Documents\Android_Projects\projects\decision2014.pushme\pushme\platforms\android\CordovaLib\AndroidManifest.xml \
