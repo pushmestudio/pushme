caa941da1179bf74795d16109be38205fc065d00
capsmalt <capsmalt@gmail.com>
2015-01-30 21:57:32 +0900

    modified Clip function
    クリップ一覧で、カテゴリALL指定時にUnClipし、そのカテゴリのアイテムがなくなった場合に、カテゴリプルダウンメニューから、対象カテゴリ名を削除。

--- pushme/platforms/android/ant-build/classes.dex.d
+++ pushme/platforms/android/ant-build/classes.dex.d
@@ -1,19 +1,16 @@
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes.dex : \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\com\android\vending\billing\IInAppBillingService$Stub$Proxy.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\com\android\vending\billing\IInAppBillingService$Stub.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\com\android\vending\billing\IInAppBillingService.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\BuildConfig.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\PushMe$JSHandler.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\PushMe.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\R$attr.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\R$drawable.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\R$string.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\R$xml.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\jp\pushme\R.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$1.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$Action.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$DBQuery.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$DBRunner.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin$QueryType.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\classes\org\pgsqlite\SQLitePlugin.class \
-C:\cygwin64\home\AA354472\gitMyDev\decision2014.pushme\pushme\platforms\android\ant-build\dexedLibs\classes-72c230f14169d4ea29ae91bf1624f22c.jar \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes.dex : \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/BuildConfig.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/PushMe$JSHandler.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/PushMe.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/R$attr.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/R$drawable.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/R$string.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/R$xml.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/jp/pushme/R.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$1.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$Action.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$DBQuery.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$DBRunner.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin$QueryType.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/classes/org/pgsqlite/SQLitePlugin.class \
+/Users/capsmalt/Documents/env/01.Projects/KSSOSS/mypushme/decision2014.pushme/pushme/platforms/android/ant-build/dexedLibs/classes-a041f5c4699c2642a25ca7874ce4cb55.jar \
