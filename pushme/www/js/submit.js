aae404aabc2e80d63eb4b2488fb3eca9df281adb
capsmalt <capsmalt@gmail.com>
2015-02-03 13:00:35 +0900

    modified notification function 通知位置を固定

--- pushme/www/js/submit.js
+++ pushme/www/js/submit.js
@@ -184,7 +184,7 @@ $('#confirmAdd').click(function(){
     name = $('#name').val();
     desc = $('#description').val();
     if( cate == "" || name == ""){
-    	  $('#addFailCuzEmptyElementExists').stop().fadeIn(1000).delay(2000).fadeOut(1000).css('color','#FF82B2');
+    	  $('#addFailCuzEmptyElementExists').stop().fadeIn(1000).delay(2000).fadeOut(1000).css('color','#000000');
     } else {
 		$('#regConfirm').html("Add the following item ?<br>【Category】: " + cate + "<br>【Subject】: " + name + "<br>【Description】: " + desc);
 		$('#regConfirm').dialog("open");
